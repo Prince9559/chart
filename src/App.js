3c0491375ddfd67f4f70f622790923a99a8d6d5d
Prince Software <kumarprince95596186@gmail.com>
2025-05-10 11:00:15 +0530

    Adding Chart line

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+ import "./App.css";
+import LineChart from "./components/LineChart";
 function App() {
+  const data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July","August","September","October","November","December"],
+    data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    title:"My Line"
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LineChart data={data} />
     </div>
   );
 }
